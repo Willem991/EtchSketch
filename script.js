ce87808c17d205065e9e145554c65da26fa12659
Willem Ruan <ruan.nieu@gmail.com>
2021-08-19 19:36:41 +0200

    Added scrollbar and buttons

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let n = 256
+const range = document.querySelector('#myRange');
+
+const rangeDisplay = document.querySelector('#rangeDisplay');
+
+range.addEventListener('click', () => {
+    rangeDisplay.textContent = range.value;
+    console.log("Hello")
+})
+
+
+let n = 256;
 let k = "div";
 
-const mainDiv = document.querySelector('#mainBlock')
+const mainDiv = document.querySelector('#mainBlock');
 
 for(let i = 0; i < n; i++){
     eval('var ' + k + i + "= document.createElement('div');");
-    eval(k + i + ".classList.add('colouring');" )
-    eval(k + i + ".addEventListener('mouseover', () => {" + k + i + ".classList.add('fill');})")
+    eval(k + i + ".classList.add('colouring');" );
+    eval(k + i + ".addEventListener('mouseover', () => {" + k + i + ".classList.add('fill');})");
     eval('mainDiv.appendChild(' + k + i + ');');
 }
 

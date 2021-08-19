db9ca54cd1f93651965cb4edbb81ec3e4d8d67ec
Willem Ruan <ruan.nieu@gmail.com>
2021-08-19 19:49:39 +0200

    Added clear functionality and clearer comments

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
+//setting the slider functionality
 const range = document.querySelector('#myRange');
 
 const rangeDisplay = document.querySelector('#rangeDisplay');
 
 range.addEventListener('click', () => {
     rangeDisplay.textContent = range.value;
-    console.log("Hello")
 })
+//
 
-
+//General variables
 let n = 256;
 let k = "div";
+let selectorValue = "black";
+//
+
+//setting the black mode button functionality
+const blackMode = document.querySelector('#blackMode');
+
+blackMode/addEventListener('click', () => {
+    selectorValue = "black";
+})
+//
+
+//setting clear button functionality
+const clearButton = document.querySelector("#clear");
+
+clearButton.addEventListener('click', () => {
+    for(let j = 0; j < n; j++){
+
+        if(selectorValue == "black"){
+        eval(k + j + ".classList.remove('fill');");
+        }
+    }
+});
+//
 
 const mainDiv = document.querySelector('#mainBlock');
 
 for(let i = 0; i < n; i++){
     eval('var ' + k + i + "= document.createElement('div');");
     eval(k + i + ".classList.add('colouring');" );
+
+    if(selectorValue == "black"){
     eval(k + i + ".addEventListener('mouseover', () => {" + k + i + ".classList.add('fill');})");
+    }
+
     eval('mainDiv.appendChild(' + k + i + ');');
 }
 
